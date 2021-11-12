5a322e89f855bed999cbf193101c4b0e11d4af08
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2021-11-11 23:38:53 -0300

    test nixos module

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
   outputs = { self, home-manager, nixpkgs }:
     let
       system = "x86_64-linux";
+      
       username = "ezequiel";
       hostname = "ramis";
 
@@ -31,99 +32,9 @@
     {
       nixosConfigurations."${hostname}" = nixpkgs.lib.nixosSystem {
         inherit system;
-        modules = [(
-          { config, pkgs, ... }:
-          {
-            imports = [ ./hardware-configuration.nix ];
-
-            nix = {
-              package = pkgs.nixFlakes;
-              extraOptions = ''
-                experimental-features = nix-command flakes
-              '';
-            };
-
-            # Use the GRUB 2 boot loader.
-            boot.loader.grub.enable = true;
-            boot.loader.grub.version = 2;
-            # boot.loader.grub.efiSupport = true;
-            # boot.loader.grub.efiInstallAsRemovable = true;
-            # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-            # Define on which hard drive you want to install Grub.
-            boot.loader.grub.device = "/dev/vda"; # or "nodev" for efi only
-
-            networking.hostName = "${hostname}"; # Define your hostname.
-
-            # Set your time zone.
-            time.timeZone = "America/Argentina/Buenos_Aires";
-
-            networking.useDHCP = false;
-            networking.interfaces.enp1s0.useDHCP = true;
-
-            # Select internationalisation properties.
-            i18n.defaultLocale = "en_US.UTF-8";
-            console = {
-              font = "Lat2-Terminus16";
-              keyMap = "es";
-            };
-
-            # Enable the X11 windowing system.
-            services.xserver.enable = true;
-
-
-            # Enable the Plasma 5 Desktop Environment.
-            services.xserver.displayManager.sddm.enable = true;
-            services.xserver.desktopManager.plasma5.enable = true;
-            
-            # Configure keymap in X11
-            services.xserver.layout = "es";
-            # services.xserver.xkbOptions = "eurosign:e";
-
-            # Enable CUPS to print documents.
-            services.printing.enable = true;
-
-            # Enable sound.
-            sound.enable = true;
-            hardware.pulseaudio.enable = true;
-
-            # Define a user account. Don't forget to set a password with ‘passwd’.
-            users.users."${username}" = {
-              isNormalUser = true;
-              extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-              initialHashedPassword = "";
-            };
-
-            users.defaultUserShell = pkgs.zsh;
-            
-            # List packages installed in system profile. To search, run:
-            # $ nix search wget
-            environment.systemPackages = with pkgs; [
-              neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-              curl
-              firefox
-              vscode
-              kitty
-              git
-            ];
-
-            nixpkgs.config.allowUnfree = true;
-
-            # List services that you want to enable:
-
-            # Enable the OpenSSH daemon.
-            services.openssh.enable = true;
-
-            # This value determines the NixOS release from which the default
-            # settings for stateful data, like file locations and database versions
-            # on your system were taken. It‘s perfectly fine and recommended to leave
-            # this value at the release version of the first install of this system.
-            # Before changing this value read the documentation for this option
-            # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-            system.stateVersion = "21.05"; # Did you read the comment?
-
-          }
-        )];
+        modules = [ ./nixos/configuration.nix ];
       };
+
       homeManagerConfigurations = home-manager.lib.homeManagerConfiguration {
         inherit pkgs system username;
         homeDirectory = "/home/${username}";
