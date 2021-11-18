415fff79636d5e2d3f3e334e0f1910d64b6bb021
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2021-11-18 18:13:15 -0300

    fixes

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -9,14 +9,12 @@
     '';
   };
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/vda"; # or "nodev" for efi only
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # vbox
+  boot.loader.grub.device = "nodev"; # or "nodev" for efi only
+  boot.initrd.checkJournalingFS = false;
 
   networking.hostName = "${hostname}"; # Define your hostname.
 
