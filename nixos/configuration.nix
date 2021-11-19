ea417cd97b3b083c3c87b382c025d0326e5b9208
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2021-11-18 21:29:45 -0300

    .

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -9,7 +9,7 @@
     '';
   };
 
-  boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
   boot.loader.grub.devices = [ "nodev" ];
   boot.loader.grub.efiInstallAsRemovable = true;
