e86ddac0b16b59e60a80e46ca281e6e11b1754aa
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2022-01-23 00:20:44 -0300

    see if it can install doom automatically

--- home/emacs/default.nix
+++ home/emacs/default.nix
@@ -1,38 +1,26 @@
 { config, lib, pkgs, ... }:
 let
-  enable = false;
-  doomSyncScript = pkgs.writeScriptBin "doom-sync-git" ''
-    #!${pkgs.runtimeShell}
-    export PATH=$PATH:${lib.makeBinPath [ pkgs.git pkgs.sqlite pkgs.unzip ]}
-    if [ ! -d $HOME/.emacs.d/.git ]; then
-      mkdir -p $HOME/.emacs.d
-      git -C $HOME/.emacs.d init
-    fi
-    if [ $(git -C $HOME/.emacs.d rev-parse HEAD) != ${pkgs.doomEmacsRevision} ]; then
-      git -C $HOME/.emacs.d fetch https://github.com/hlissner/doom-emacs.git || true
-      git -C $HOME/.emacs.d checkout ${pkgs.doomEmacsRevision} || true
-    fi
-  '';
-in;
+in
 {
-  # programs.doom-emacs = {
-  #   enable = false;
-  #   doomPrivateDir = ./doom;
-  #   emacsPackage = pkgs.emacsPgtkGcc;
-  # };
-  home.sessionVariables = {
-    DOOMDIR = ./doom;
-    PATH = [ "$HOME/.emacs.d/bin" ];
+  programs.emacs = {
+    enable = false;
+    package = pkgs.emacsPgtkGcc;
   };
+
+  home.sessionVariables.DOOMDIR = ./doom;
+  home.sessionPath = [ "$HOME/.emacs.d/bin" ];
 
   home.packages = with pkgs; [
     fd
     sqlite
-    doomSyncScript
   ];
 
-  services.emacs = {
-    inherit enable;
-    package = pkgs.emacsPgtkGcc;
+  home.activation = {
+    doom-sync = lib.hm.dag.entryAfter [] ''
+      mkdir -p "$HOME/.emacs.d"
+      git -C "$HOME/.emacs.d"
+      git -C $HOME/.emacs.d fetch https://github.com/hlissner/doom-emacs.git || true
+      git -C $HOME/.emacs.d checkout ${pkgs.doomEmacsRevision} || true
+    '';
   };
 }
