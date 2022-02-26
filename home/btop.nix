bb751ddd758c281ce71e5a7ad7ba2d4c8d52c52b
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2022-02-26 10:34:12 -0300

    btop configured

--- home/btop.nix
+++ home/btop.nix
@@ -1,4 +1,13 @@
 { config, lib, pkgs, ... }:
 {
   home.packages = with pkgs; [ btop ];
+  xdg.configFile."btop/btop.conf".text = ''
+    color_theme = "TTY"
+    theme_background = False
+    truecolor = False
+    vim_keys = True
+    rounded_corners = True
+    graph_symbol = "braille"
+    update_ms = 200
+  '';
 }
