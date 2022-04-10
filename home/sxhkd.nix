7851dec283eb8f5fdab7553d98d081828347c347
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2022-04-10 16:00:43 -0300

    some sxhkd settings

--- home/sxhkd.nix
+++ home/sxhkd.nix
@@ -13,19 +13,46 @@ let
   plus = prefix " + ";
   chord = prefix " ; ";
   chord' = prefix " : ";
+  step = "10";
+  step' = 10;
 in {
   services.sxhkd = {
     enable = true;
     keybindings = mkHotkeyChain {
       "alt + {_,shift + }Tab" = "bspc node -f {next,prev}.leaf";
+
       super = plus {
-        # reload sxhkd
-        shift = plus { Escape = "pkill -USR1 -x sxhkd"; };
-        "{w,a,s,d}" = "bspc node -f {north,west,south,east}.leaf";
         Return = "$TERMINAL";
         space = "rofi -show run";
         f = "bspc desktop -l next";
         p = "bspc node -g sticky";
+        m = "bspc node @/first -f";
+        k = "bspc node -c";
+        "{q,e}" = "bspc node @parent -F {horizontal,vertical}";
+        "{w,a,s,d}" = "bspc node -f {north,west,south,east}.leaf";
+
+        shift = plus {
+          # reload sxhkd
+          Escape = "pkill -USR1 -x sxhkd";
+          k = "bspc node -k";
+          "{q,e}" = "bspc node @parent -R {270,90}";
+          "{w,a,s,d}" = ''
+          {\
+            bspc node @parent/first -z bottom 0 -${step}; \
+            bspc node @parent/second -z top 0 -${step}, \
+            bspc node @parent/second -z left -${step} 0; \
+            bspc node @parent/first -z right -${step} 0, \
+            bspc node @parent/second -z top 0 +${step}; \
+            bspc node @parent/first -z bottom 0 +${step}, \
+            bspc node @parent/first -z right +${step} 0; \
+            bspc node @parent/second -z left +${step} 0 \
+            }'';
+        };
+
+        alt = plus {
+          "{q,e}" = "bspc node @parent -C {backward,forward}";
+          "{w,a,s,d}" = "bspc node -s {north,west,south,east}.leaf";
+        };
       };
     };
   };
